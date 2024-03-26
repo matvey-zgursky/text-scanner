e68ba31fb2c0e886576cfecddd7efa3c05646836
matvey-zgursky <matveyzgursky@gmail.com>
2024-03-26 19:25:14 +0700

    завершен функционал по открыванию проводника по кнопке на стартовой странице

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import scanner
 from app import App
 
 def main():
-    app = App()
-    app.start()
-    #scanner.print_text('image.jpg')
+    try:
+        app = App()
+        app.start()
+        #scanner.print_text('image.jpg')
+    except Exception as ex:
+        print(f"Error: {ex}")
 
 if __name__ == '__main__':
     main()
